7afac2bffbf3c17d392f905ef55e31c30ebab0d9
Avraham Levi <lwybrhm048@gmail.com>
2025-05-27 11:21:48 +0300

    new feuter Weapon

--- PracticSimpleClass/Program.cs
+++ PracticSimpleClass/Program.cs
@@ -10,7 +10,10 @@ namespace PracticSimpleClass
     {
         static void Main(string[] args)
         {
-
+            Commando c = new Commando("Avi", "Levi", "Hide");
+            Weapon w = new Weapon("Glook", "Austria", 13);
+            w.Shoot();
+            Console.WriteLine(w.NumBools);
         }
     }
 }
---
+++ PracticSimpleClass/Weapon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticSimpleClass
+{
+    internal class Weapon
+    {
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public int NumBools { get; set; }
+
+        public Weapon(string name, string manufacturer, int numBools) 
+        {
+            Name = name; 
+            Manufacturer = manufacturer;
+            NumBools = numBools;
+        }
+        public void Shoot()
+        {
+            if(NumBools > 0)
+            {
+                Console.WriteLine("Boom");
+                NumBools -= 1;
+            }
+            else
+            {
+                Console.WriteLine("The ball is over ");
+            }
+        }
+
+    }
+}
