808e90097502168b9bf8a83b4841cdce0222a13a
Avraham Levi <lwybrhm048@gmail.com>
2025-05-27 10:25:02 +0300

    Start

---
+++ PracticSimpleClass/Commando.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticSimpleClass
+{
+    internal class Commando
+    {
+        public string Name { get; set; }
+        public string CodeName { get; set; }
+        public string Status { get; set; }
+        public IReadOnlyList <string> Tools { get; } = new List<string>{ "Hammer", "Chisel", "Rope", "Bag", "Canteen" };
+
+        public Commando(string name, string codeName, string status) 
+        {
+            Name = name;
+            CodeName = codeName;
+            Status = status;
+            
+
+        }
+        public void Walk()
+        {
+            Status = "Walk";
+            Console.WriteLine($"The soldier {Name} is: {Status}");
+        }
+        public void Hide() 
+        {
+            Status = "Hide";
+            Console.WriteLine($"The soldier {Name} is: {Status}");
+        }
+        public void Attack()
+        {
+
+        }
+
+
+
+    }
+}
